cb73255fab7c6f0d7a416a0258a0e421d784688b
sunilbist <sunilbdrbist@yahoo.com>
2025-06-08 12:13:52 +0545

    signup page handled

--- app/src/main/java/np/edu/nast/be7thg1app/SignupActivity.java
+++ app/src/main/java/np/edu/nast/be7thg1app/SignupActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,15 +19,27 @@ public class SignupActivity extends AppCompatActivity {
     private EditText etFirstName, etLastName, etEmail, etPassword, etConfirmPassword;
     private Button btnSignup, btnLogin;
 
+    private RadioButton rdoMale, rdoFrmale;
+
+    private TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
         etFirstName = findViewById(R.id.et_first_name);
         etLastName = findViewById(R.id.et_last_name);
+        etEmail = findViewById(R.id.et_email);
+        etPassword = findViewById(R.id.et_password);
+        etConfirmPassword = findViewById(R.id.et_confirm_password);
+        rdoFrmale = findViewById(R.id.rdo_female);
+        rdoMale = findViewById(R.id.rdo_male);
+        tvResult = findViewById(R.id.tv_result);
 
         btnLogin = findViewById(R.id.btn_login);
+        btnSignup = findViewById(R.id.btn_signup);
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -33,5 +47,14 @@ public class SignupActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String firsName = etFirstName.getText().toString();
+                tvResult.setText(firsName);
+            }
+        });
+        //added
     }
 }
