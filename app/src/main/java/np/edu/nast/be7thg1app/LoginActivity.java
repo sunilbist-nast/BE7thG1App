8ef47f103139081f57de48b173fb3af2d4489e37
sunilbist-nast <sunil@nast.edu.np>
2025-06-08 11:05:10 +0545

    message

---
+++ app/src/main/java/np/edu/nast/be7thg1app/LoginActivity.java
@@ -0,0 +1,45 @@
+package np.edu.nast.be7thg1app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText etUsername, etPassword;
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etUsername = findViewById(R.id.etUser);
+        etPassword = findViewById(R.id.etPass);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = etUsername.getText().toString();
+                String pass = etPassword.getText().toString();
+                if (user.equals("BE") && pass.equals("computeR")) {
+                    Intent intent= new Intent(LoginActivity.this,MainActivity.class);
+                    startActivity(intent);
+
+                }else{
+                    Toast.makeText(getApplicationContext(),"Username/Password not matched",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/np/edu/nast/be7thg1app/SignupActivity.java
@@ -0,0 +1,18 @@
+package np.edu.nast.be7thg1app;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
